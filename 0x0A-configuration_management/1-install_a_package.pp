150962b7934937fa2dffe37ec560bb18bc7fe129
opegol <opgbenga@gmail.com>
2024-03-22 22:23:40 +0000

    Edits to 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install flask from pip3 using puppet
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['werkzeug']
 }
