d6f7ef88c6ed8b3137a7b5c2b1679d5f70a689de
opegol <opgbenga@gmail.com>
2024-03-24 23:08:21 +0000

    Edit of 100-puppet ssh task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Editing ssh config file using puppet
 
-file_line { 'id_key':
-  ensure => 'present',
-  path   => '/etc/ssh/sshd_config',
-  line   => '    IdentityFile ~/.ssh/school',
-}
-
-file_line { 'no_psw':
-  ensure => 'present',
-  path   => '/etc/ssh/sshd_config',
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   match  => '^#\s*PasswordAuthentication\s[a-z]*'
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school'
 }
