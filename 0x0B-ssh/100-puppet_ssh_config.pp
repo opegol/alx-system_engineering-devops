f16de33d51138f7dc06dde2b1138e599885c164a
opegol <139570017+opegol@users.noreply.github.com>
2024-03-25 00:26:46 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,13 @@
 
 file_line { 'id_key':
   ensure => 'present',
-  path   => '/etc/ssh/sshd_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
 }
 
 file_line { 'no_psw':
   ensure => 'present',
-  path   => '/etc/ssh/sshd_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   match  => '^#\s*PasswordAuthentication\s[a-z]*'
 }
